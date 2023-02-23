f81c2e5de0de35e021ef0d6c3f2fdfd130b49535
Daria Yakkola <yakkolabest@gmail.com>
2023-02-24 01:50:10 +0300

    Задача 27

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string array = number.ToString();
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+  int result = int.Parse(char.ToString(array[i]));
+  sum = sum + result;
+}
+Console.WriteLine($"Cумма цифр в числе {number} равна {sum} ");
